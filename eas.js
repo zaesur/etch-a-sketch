cecebf8bf39245962302a13662a58cf3b37719d8
Florian Van Belleghem <fvanbelleghem@icloud.com>
2019-01-05 23:24:22 +0100

    added missing parentheses.

--- eas.js
+++ eas.js
@@ -1,7 +1,7 @@
 const dimension = 16;
 const container = document.querySelector('#container');
-container.style.setProperty('grid-template-rows', `repeat(${dimension}, 10px`);
-container.style.setProperty('grid-template-columns', `repeat(${dimension}, 10px`);
+container.style.setProperty('grid-template-rows', `repeat(${dimension}, 10px)`);
+container.style.setProperty('grid-template-columns', `repeat(${dimension}, 10px)`);
 for (let step = 0; step < dimension ** 2; step++) {
   const pixel = document.createElement('div');
   pixel.classList.add('pixel');
