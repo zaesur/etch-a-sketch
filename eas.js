fd7cc6483208b0930710689ba362cdcac0c5e024
Florian Van Belleghem <fvanbelleghem@icloud.com>
2019-01-06 20:00:18 +0100

    finished core functionality

--- eas.js
+++ eas.js
@@ -1,14 +1,34 @@
-const dimension = 16;
 const container = document.querySelector('#container');
-container.style.setProperty('grid-template-rows', `repeat(${dimension}, 10px)`);
-container.style.setProperty('grid-template-columns', `repeat(${dimension}, 10px)`);
-for (let step = 0; step < dimension ** 2; step++) {
-  const pixel = document.createElement('div');
-  pixel.classList.add('pixel');
-  container.appendChild(pixel);
+let dimension = 16;
+
+document.querySelector('#size').addEventListener('click', e => {
+  const newDimension = parseInt(prompt("Please enter a grid size", dimension.toString()));
+  if (newDimension === dimension || !newDimension) return;
+  dimension = newDimension;
+  updateGrid();
+});
+
+function makePixels(dimension) {
+  container.style.setProperty('grid-template', `repeat(${dimension}, 1fr) / repeat(${dimension}, 1fr)`)
+  for (let step = 0; step < dimension ** 2; step++) {
+    const pixel = document.createElement('div');
+    pixel.classList.add('pixel');
+    pixel.addEventListener('mouseover', e => {
+      const opacity = getComputedStyle(e.target).getPropertyValue('opacity');
+      e.target.style.setProperty('opacity', `${Math.min(parseFloat(opacity) + 0.1, 1)}`);
+    })
+    container.appendChild(pixel);
+  }
 }
-pixels = document.querySelectorAll('.pixel');
-pixels.forEach(pixel => pixel.addEventListener('mouseover', e => {
-  const opacity = getComputedStyle(e.target).getPropertyValue('opacity');
-  e.target.style.setProperty('opacity', `${Math.min(parseFloat(opacity) + 0.1, 1)}`);
-}));
+
+function clearPixels() {
+  const pixels = container.querySelectorAll('.pixel');
+  pixels.forEach(pixel => container.removeChild(pixel));
+}
+
+function updateGrid() {
+  clearPixels();
+  makePixels(dimension);
+}
+
+makePixels(dimension);
